7f7f0a862243cb8dc4c979713a2ad9fd0d996db3
DIMITRIOS MARKANTONATOS <dim13_94@hotmail.com>
2020-03-27 16:02:18 +0200

    test json data

--- upload.php
+++ upload.php
@@ -1,6 +1,23 @@
 <?php
-if (isset($_FILES['uploadingfile'])){
-    $msg = array("msg" => "file ok");
+include_once ('connection.php');
 
+if (isset($_FILES['uploadingfile'])){
+
+    $file = $_FILES['uploadingfile']['tmp_name'];
+    print_r($file);
+
+
+    $data = file_get_contents($file);
+
+    $array = json_decode($data, true );
+
+    print_r($array);
+
+    foreach ($array["locations"] as $location ) {
+            echo $location['timestampMs'];
+            echo PHP_EOL;
+        }
+
+    $msg = array("msg" => "times ok ");
     exit(json_encode($msg));
 }
