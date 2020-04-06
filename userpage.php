138fb22b78fa668681f444faff4de31728093210
DIMITRIOS MARKANTONATOS <dim13_94@hotmail.com>
2020-04-06 18:36:23 +0300

    upload json only patras locations and timestamps and button

--- userpage.php
+++ userpage.php
@@ -11,17 +11,56 @@ if(!isset($_SESSION['uiduser'])){
 
 
 <head>
-    <title>LogInPageΘσερ</title>
+    <title>LogInPageUser</title>
     <meta name="viewport" http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"/>
     <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"> </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Month', 'Score'],
+                ['January',  10],
+                ['February',  24],
+                ['March',  23],
+                ['April',  50],
+                ['May',  50],
+                ['June',  63],
+                ['Juy',  30],
+                ['August',  80],
+                ['September',  23],
+                ['October',  12],
+                ['November',  10],
+                ['December',  18]
+            ]);
+
+            var options = {
+                title: 'Your scores this last year',
+                curveType: 'function',
+                legend: { position: 'bottom' }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+            chart.draw(data, options);
+        }
+    </script>
+    <link rel="stylesheet" href="userpage.css">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <script src="js/vendor/jquery.ui.widget.js"></script>
     <script src="js/jquery.iframe-transport.js"></script>
     <script src="js/jquery.fileupload.js"></script>
     <script src="upload.js"></script>
-    <link rel="stylesheet" href="userpage.css">
+
 </head>
 <body background="images/rio.jpg">
 <header>
@@ -35,18 +74,48 @@ if(!isset($_SESSION['uiduser'])){
 
 <main>
     <div id="mapcontainer">
-    <a id="maptitle">Upload your location json file for Patras city</a>
-    <div id="mapid"></div>
-    <script type="text/javascript" src="map.js"></script>
-    <div id="upload" >
-        Choose your json file :
-        <input type="file" name="uploadingfile" id="uploadedfile" accept=".json" required/>
-        <button name="submitupload" id="submitupload">Sumbit your JSON file!</button>
-        <a id="progress"></a><br>
-        <div id="error"></div><br>
-        <div id="files"></div>
+        <a id="maptitle">Upload your location json file for Patras city</a>
+        <div id="mapid"></div>
+        <script type="text/javascript" src="map.js"></script>
+        <div id="upload" >
+            Choose your json file :
+            <input type="file" name="uploadingfile" id="uploadedfile" accept=".json" required/>
+            <button name="submitupload" id="submitupload">Sumbit your JSON file!</button>
+            <a id="progress"></a><br>
+            <div id="error"></div><br>
+            <div id="files"></div>
+        </div>
     </div>
+
+    <div class="datepicker">
+        <h3>Select your range of dates</h3>
+        <input type="text" name="datefilter" value="" />
+
+        <script type="text/javascript">
+            $(function() {
+
+                $('input[name="datefilter"]').daterangepicker({
+                    autoUpdateInput: false,
+                    locale: {
+                        cancelLabel: 'Clear'
+                    }
+                });
+
+                $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+                    $(this).val(picker.startDate.format('MM/YYYY') + ' - ' + picker.endDate.format('MM/YYYY'));
+                });
+
+                $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+                    $(this).val('');
+                });
+
+            });
+        </script>
     </div>
+
+
+
+
 
     <div id="highscores">
         <a id="highscoresmsg">These are the top three ecologists of the month</a>
@@ -74,8 +143,39 @@ if(!isset($_SESSION['uiduser'])){
 
         </table>
 
+        <div id="curve_chart" style="width: 700px; height: 300px"></div>
+
+        <!-- Apikonisi stixion xristi -->
+        <div class="container2">
+            <div class="box">
+                <div class="icon"><i class="fa fa-leaf" aria-hidden="true"></i></div>
+                <div class="content">
+                    <h3>Eco Score</h3>
+                    <p> Your eco score this past month is ...%</p>
+
+                </div>
+            </div>
+
+            <div class="box2">
+                <div class="icon2"><i class="fa fa-address-book" aria-hidden="true"></i></div>
+                <div class="content2">
+                    <h3>User Info</h3>
+                    <p> Your last upload was on ""</p>
+                    <p> Your data covers those dates: ""</p>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+
+
 
     </div>
+
+
 
 </main>
 
