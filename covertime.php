dfb7616743b16234da9f7518d5172ffbbef576ab
nmargaris <nmargaris@ceid.upatras.gr>
2020-04-13 17:22:23 +0300

    allagi

--- covertime.php
+++ covertime.php
@@ -2,3 +2,16 @@
 include_once ('connection.php');
 session_status();
 $tempusername = $_SESSION['iduser'];
+$date_data= array();
+
+$sql = "SELECT timestamp FROM locations WHERE username='$tempusername' ORDER BY timestamp ";
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result) )
+{
+    $date_data [] = array(
+        'timestamp'  => $row['timestamp']
+    ) ;
+
+}
+
+return json_decode($date_data);
