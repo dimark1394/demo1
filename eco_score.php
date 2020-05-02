8943644773a24667bd2a590913039cc24bd9b018
nmargaris <nmargaris@ceid.upatras.gr>
2020-05-02 20:29:33 +0300

    allagi

--- eco_score.php
+++ eco_score.php
@@ -4,14 +4,14 @@ session_start();
 $tempusername = $_SESSION['uiduser'];
 
 date_default_timezone_set('Europe/Athens');
-$currnet_moth=date('m');
+$current_month=date('m');
 $count_loc=0;
 
 $count_eco=0;
 
 $locations=array();
 
-$sql="SELECT * FROM locations WHERE timestamp = MONTH(CURRENT_TIMESTAMP) AND timestamp = YEAR(CURRENT_TIMESTAMP) AND username='$tempusername' ";
+$sql="SELECT * FROM locations WHERE MONTH(timestamp) = MONTH(CURRENT_DATE()) AND YEAR(timestamp ) = YEAR(CURRENT_DATE()) ";
 $result=mysqli_query($conn, $sql);
 //arithmos topothesiwn
 $count_loc=mysqli_num_rows($result);
@@ -22,7 +22,7 @@ if ($count_loc == 0)
 }
 else
 {
-    $query="SELECT * FROM activity WHERE timestamp = MONTH(CURRENT_TIMESTAMP) AND timestamp = YEAR(CURRENT_TIMESTAMP) AND username='$tempusername' AND (type='WALKING' OR type='ON_BICYCLE' OR type='ON_FOOT' OR type='RUNNING')";
+    $query="SELECT * FROM activity WHERE MONTH(timestamp) = MONTH(CURRENT_DATE()) AND YEAR(timestamp ) = YEAR(CURRENT_DATE()) AND username='$tempusername' AND (type='WALKING' OR type='ON_BICYCLE' OR type='ON_FOOT' OR type='RUNNING')";
     //arithmos oikologikwn type
     $result=mysqli_query($conn, $query);
     $count_eco=mysqli_num_rows($result);
@@ -32,18 +32,10 @@ else
     echo "Your eco score this month is ", $eco_score , "%";
 }
 
-/*echo $count_loc;
+/*echo "Arithmos topothesiwn ",$count_loc;
 echo "<br>";
-echo  $currnet_moth;
-*/
+echo  $current_month; */
 
-/*
-$link = mysql_connect("localhost", "mysql_user", "mysql_password");
-mysql_select_db("database", $link);
 
-$result = mysql_query("SELECT * FROM table1", $link);
-$num_rows = mysql_num_rows($result);
 
-echo "$num_rows Rows\n";
- */
 
