a239c2f98018f64f1c50311cf229203c79f664ba
Dimitris Markantonatos <dimark1382@gmail.com>
2020-05-06 19:16:03 +0300

    ajax for data date spans

--- datespan.js
+++ datespan.js
@@ -6,13 +6,15 @@ $(document).ready(function () {
             dataType: 'JSON',
             success: function (data) {
                 var response = JSON.parse(JSON.stringify(data));
-                document.getElementById("lastupload").innerText = response[0].lastupdt;
-                document.getElementById("startdate").innerText = " Your data cover from: " + response[0].startdt;
-                document.getElementById("enddate").innerText = " To: " + response[0].startdt;
-                $("#lastupload").text =  response[0].lastupdt;
-                console.log(response[0].lastupdt);
-                console.log(response[0].startdt);
-                console.log(response[0].lastdt);
+                if (response[0].lastupdt === 'You have not uploaded data yet'){
+                    document.getElementById("lastupload").innerText = response[0].lastupdt;
+                }
+                else {
+                    document.getElementById("lastupload").innerText = response[0].lastupdt;
+                    document.getElementById("startdate").innerText = " Your data cover from: " + response[0].startdt;
+                    document.getElementById("enddate").innerText = " To: " + response[0].startdt;
+                    $("#lastupload").text = response[0].lastupdt;
+                }
             }
         })
     })
