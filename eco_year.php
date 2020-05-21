fa3715828c2f6b72aa52ed5995f20c7e95731ac9
Dimitris Markantonatos <dimark1382@gmail.com>
2020-05-21 17:52:53 +0300

    entries per activity

--- eco_year.php
+++ eco_year.php
@@ -7,86 +7,87 @@ date_default_timezone_set('Europe/Athens');
 
 $eco_type=array();
 
-$sql="SELECT * FROM activity WHERE timestamp BETWEEN DATE_SUB(NOW(), INTERVAL 365 DAY) AND NOW()  ";
+$sql="SELECT type,MONTH(timestamp),count(*) AS counter FROM activity  GROUP BY type,MONTH(timestamp)";
 $result=mysqli_query($conn, $sql);
 
 while($row = mysqli_fetch_array($result) )
 {
     $eco_type [] = array(
-        'timestamp' => $row['timestamp'],
-        'type'  => $row['type']
-
+        'type' => $row['type'],
+        'count' => $row['counter'],
+        'month' => $row['MONTH(timestamp)']
     ) ;
 }
+print_r($eco_type);
 
-$N = sizeof($eco_type);
-
-for($i=0; $i<$N; $i++ )
-{
-
-    $eco_type[$i]['timestamp']=substr($eco_type[$i]['timestamp'], 5,2);
-   // echo $eco_type[$i]['timestamp'];
-    //echo "<br>";
-}
-
-for($i=0; $i<$N; $i++ )
-{
-    if ($eco_type[$i]['timestamp']=="01")
-    {
-        $eco_type[$i]['timestamp'] = "January";
-    }
-    elseif ($eco_type[$i]['timestamp']=="02")
-    {
-        $eco_type[$i]['timestamp'] = "February";
-    }
-    elseif ($eco_type[$i]['timestamp']=="03")
-    {
-        $eco_type[$i]['timestamp'] = "March";
-    }
-    elseif ($eco_type[$i]['timestamp']=="04")
-    {
-        $eco_type[$i]['timestamp'] = "April";
-    }
-    elseif ($eco_type[$i]['timestamp']=="05")
-    {
-        $eco_type[$i]['timestamp'] = "May";
-    }
-    elseif ($eco_type[$i]['timestamp']=="06")
-    {
-        $eco_type[$i]['timestamp'] = "June";
-    }
-    elseif ($eco_type[$i]['timestamp']=="07")
-    {
-        $eco_type[$i]['timestamp'] = "July";
-    }
-    elseif ($eco_type[$i]['timestamp']=="08")
-    {
-        $eco_type[$i]['timestamp'] = "August";
-    }
-    elseif ($eco_type[$i]['timestamp']=="09")
-    {
-        $eco_type[$i]['timestamp'] = "September";
-    }
-    elseif ($eco_type[$i]['timestamp']=="10")
-    {
-        $eco_type[$i]['timestamp'] = "October";
-    }
-    elseif ($eco_type[$i]['timestamp']=="11")
-    {
-        $eco_type[$i]['timestamp'] = "November";
-    }
-    elseif ($eco_type[$i]['timestamp']=="12")
-    {
-        $eco_type[$i]['timestamp'] = "December";
-    }
-}
-
-for($i=0; $i<$N; $i++ )
-{
-    echo $eco_type[$i]['timestamp'];
-    echo "<br>";
-}
-
-echo json_encode($eco_type);
-
-
+//$N = sizeof($eco_type);
+//
+//for($i=0; $i<$N; $i++ )
+//{
+//
+//    $eco_type[$i]['timestamp']=substr($eco_type[$i]['timestamp'], 5,2);
+//   // echo $eco_type[$i]['timestamp'];
+//    //echo "<br>";
+//}
+//
+//for($i=0; $i<$N; $i++ )
+//{
+//    if ($eco_type[$i]['timestamp']=="01")
+//    {
+//        $eco_type[$i]['timestamp'] = "January";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="02")
+//    {
+//        $eco_type[$i]['timestamp'] = "February";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="03")
+//    {
+//        $eco_type[$i]['timestamp'] = "March";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="04")
+//    {
+//        $eco_type[$i]['timestamp'] = "April";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="05")
+//    {
+//        $eco_type[$i]['timestamp'] = "May";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="06")
+//    {
+//        $eco_type[$i]['timestamp'] = "June";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="07")
+//    {
+//        $eco_type[$i]['timestamp'] = "July";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="08")
+//    {
+//        $eco_type[$i]['timestamp'] = "August";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="09")
+//    {
+//        $eco_type[$i]['timestamp'] = "September";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="10")
+//    {
+//        $eco_type[$i]['timestamp'] = "October";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="11")
+//    {
+//        $eco_type[$i]['timestamp'] = "November";
+//    }
+//    elseif ($eco_type[$i]['timestamp']=="12")
+//    {
+//        $eco_type[$i]['timestamp'] = "December";
+//    }
+//}
+//
+//for($i=0; $i<$N; $i++ )
+//{
+//    echo $eco_type[$i]['timestamp'];
+//    echo "<br>";
+//}
+//
+//echo json_encode($eco_type);
+//
+//
