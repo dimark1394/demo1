c16358a2a4f0c05d524646abb165e184c55b62c8
nmargaris <nmargaris@ceid.upatras.gr>
2020-06-04 18:07:10 +0300

    allagi

--- top3scores.php
+++ top3scores.php
@@ -12,10 +12,10 @@ while($row = mysqli_fetch_array($result) )
         'count' => 0
     ) ;
 }
-echo  "THis the users array";
-echo "<br>";
-print_r($users);
-echo "<br>";
+//echo  "THis the users array";
+//echo "<br>";
+//print_r($users);
+//echo "<br>";
 $sql1="SELECT username,count(*) AS counter FROM activity WHERE MONTH(timestamp) = MONTH(CURRENT_DATE()) AND YEAR(timestamp ) = YEAR(CURRENT_DATE()) AND (type='WALKING' OR type='ON_BICYCLE' OR type='ON_FOOT' OR type='RUNNING') GROUP BY username";
 $result1=mysqli_query($conn, $sql1);
 while($row = mysqli_fetch_array($result1) )
@@ -25,10 +25,10 @@ while($row = mysqli_fetch_array($result1) )
         'count' => $row['counter']
     ) ;
 }
-echo "<br>";
-echo  "THis the scores for each month";
-echo "<br>";
-print_r($scores);
+//echo "<br>";
+//echo  "THis the scores for each month";
+//echo "<br>";
+//print_r($scores);
 
 $n=sizeof($users);
 $m=sizeof($scores);
@@ -46,10 +46,10 @@ for($i=0; $i<$m; $i++)
     }
 }
 
-echo "<br>";echo "<br>";
-echo  "THis the scores for each month final";
-echo "<br>";
-print_r($users);
+//echo "<br>";echo "<br>";
+//echo  "THis the scores for each month final";
+//echo "<br>";
+//print_r($users);
 
 //menei na ta sortarw to users se ftinousa seira me to keyvalye count
 function sortByOrder($a, $b) {
@@ -60,7 +60,48 @@ usort($users, 'sortByOrder');
 
 
 
-echo "<br>";echo "<br>";
-echo  "THis the scores for each month final";
-echo "<br>";
-print_r($users);
+//echo "<br>";echo "<br>";
+//echo  "THis the scores for each month before including the the top 3  user";
+//echo "<br>";
+//print_r($users);
+
+
+$users_final= array();
+
+$users_final[0]['user']=$users[0]['user'];
+$users_final[0]['count']=$users[0]['count'];
+$users_final[0]['position']=1;
+$users_final[1]['user']=$users[1]['user'];
+$users_final[1]['count']=$users[1]['count'];
+$users_final[1]['position']=2;
+$users_final[2]['user']=$users[2]['user'];
+$users_final[2]['count']=$users[2]['count'];
+$users_final[2]['position']=3;
+
+
+//echo "<br>";echo "<br>";
+//echo  "THis the scores for each month before including only the the top 3  user";
+//echo "<br>";
+//print_r($users_final);
+//
+//$n=sizeof($users);
+
+if($tempusername!=$users_final[0]['user'] && $tempusername!=$users_final[1]['user'] && $tempusername!=$users_final[2]['user'] )
+{
+    for($i=0; $i<$n; $i++)
+    {
+        if ($users[$i]['user']==$tempusername)
+        {
+            $users_final[3]['user']=$users[$i]['user'];
+            $users_final[3]['position']=$i+1;
+        }
+    }
+}
+
+//echo "<br>";echo "<br>";
+//echo  "THis if the final final top 3 (or more) users  ";
+//echo " " .$tempusername;
+//echo "<br>";
+print_r($users_final);
+
+
